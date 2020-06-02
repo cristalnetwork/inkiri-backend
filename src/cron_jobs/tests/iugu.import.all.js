a728efb8eb479131c43a6ad2e7c119767ddd70a8
tuti <ptutino@gmail.com>
2020-06-02 16:13:53 -0300

    New importer to run once a day.

--- src/cron_jobs/tests/iugu.import.all.js
+++ src/cron_jobs/tests/iugu.import.all.js
@@ -7,9 +7,9 @@ const IuguModel      = require('../../iugu/models/iugu.model');
   console.log('iugu.import.all::BEGIN --------------------------------------------')
 
   // const import_result = await importer.importAndNotSave(50);
-  const import_result = await importer.importAllSince(10);
+  // const import_result = await importer.importAllSince(10);
   // const import_result = await importer.importAll();
-  // const import_result = await IuguModel.listUnprocessed();
+  const import_result = await IuguModel.listUnprocessed();
   
   console.log('----LEN:', (import_result||[]).length);        
   console.log(import_result);        
